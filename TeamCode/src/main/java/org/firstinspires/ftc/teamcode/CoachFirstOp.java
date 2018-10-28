13100c06f3b0a665672ea8f0d628820878ed2bb1
justgeneit <justgeneit@gmail.com>
2018-10-28 15:59:19 -0400

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoachFirstOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoachFirstOp.java
@@ -52,7 +52,7 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="OpMode1", group="Opmode")
 //@Disabled
-public class MyFirstOp extends LinearOpMode {
+public class CoachFirstOp extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawArm.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -48,11 +48,11 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@TeleOp(name = "Concept: Scan Servo", group = "Concept")
-@Disabled
-public class ConceptScanServo extends LinearOpMode {
+@TeleOp(name = "Concept: Rotate Servo", group = "Servo")
+//@Disabled
+public class clawArm extends LinearOpMode {
 
-    static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
+    static double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
     static final int    CYCLE_MS    =   50;     // period of each cycle
     static final double MAX_POS     =  1.0;     // Maximum rotational position
     static final double MIN_POS     =  0.0;     // Minimum rotational position
@@ -74,6 +74,11 @@ public class ConceptScanServo extends LinearOpMode {
         telemetry.addData(">", "Press Start to scan Servo." );
         telemetry.update();
         waitForStart();
+
+        if(gamepad2.a)
+
+
+
 
 
         // Scan servo till stop pressed.
